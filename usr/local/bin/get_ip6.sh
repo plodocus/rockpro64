1eb4cf38395df1bd606fea07617905ce5d5a2700
Daniel Hähnke <haehnke@gmail.com>
2020-04-14 12:09:46 +0200

    exclude local addresses in ip6 getter

--- usr/local/bin/get_ip6.sh
+++ usr/local/bin/get_ip6.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Gets first current global IP6
-ip6=$(/sbin/ip -6 addr show dev eth0 scope global | grep -P '(?<=inet6 ).*(?=/)' -o -m1)
+# Gets first current non-local global IP6 of eth0
+ip6=$(/sbin/ip -6 addr show dev eth0 scope global | grep -P '(?<=inet6 )(?!fd00|fe80).*(?=/)' -o -m1)
 echo $ip6
